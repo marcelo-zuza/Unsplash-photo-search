9382bfccc02ee7d23122961b2c6e69eb067a47d1
Marcelo Zuza <dev.marcelo.zuza@gmail.com>
2024-02-09 16:18:18 -0300

    3commit

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Photo = (props: Props) => {
   return (
     <div onClick={() => props.setZoomInPhoto(props.dados)}>
-        <img src={props.dados.urls.small} alt={props.dados.alt_description} className="py-4 px-2" />
+        <img src={props.dados.urls.small} alt={props.dados.alt_description} className="py-4 px-2 cursor-pointer" />
     </div>
   )
 }
